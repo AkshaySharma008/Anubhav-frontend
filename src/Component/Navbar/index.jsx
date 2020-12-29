4cd467aa84ea8ccd3649618e20ab7dfd6214f4ba
arkumish <arpit.cse24@gmail.com>
2020-12-29 23:38:18 +0530

    added navbar

--- src/Component/Navbar/index.jsx
+++ src/Component/Navbar/index.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './index.scss'
 
 const Navbar = () => {
     return (
         <>
-        <div className="d-flex justify-content-around mx-2 p-2 border-bottom">
-            <h4 className="p-2 flex-grow-1 bd-highlight">Anubhav : Experience sharing corner</h4>
+        <div className="d-flex justify-content-around mx-2 p-2 border-bottom navbar">
+            <Link to='/' className="flex-grow-1 bd-highlight title">Anubhav</Link>
 
-            <Link to='/' className="p-2">Home</Link>
-            <Link to='/write' className="p-2">Write Article</Link>
-            <Link to='/request' className="p-2">Request Article</Link>
+            <div className="action-buttons">
+            
+            <Link to='/write' className="btn btn-primary">Write Article</Link>
+            <Link to='/request' className="mx-2 btn btn-outline-primary">Request Article</Link>
+            <Link to='/guidelines'> <i className="fa fa-question-circle-o" aria-hidden="true"></i></Link>
+           
+          
+            </div>
            
         </div>
         </>
