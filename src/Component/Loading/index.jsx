c7dada363dfe8b44574db0007db04070619ece1e
arkumish <arpit.cse24@gmail.com>
2020-12-29 21:29:37 +0530

    added api call at homePage

---
+++ src/Component/Loading/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loading = () => {
+    return (
+        <>
+          Loading.....  
+        </>
+    )
+}
+
+export default Loading
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,54 +1,53 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState, useEffect, useCallback } from 'react'
 import { Link } from 'react-router-dom'
+import Loading from '../../Component/Loading';
 
 const HomePage = () => {
+    const [companyList, setCompanyList] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const loadData = useCallback(() => {
+        const apiUrl = '/api/v1/article/getAllCompanies';
+        axios.get(apiUrl).then((res) => {
+            console.log(res.data, res.data.data)
+            setCompanyList(res.data.data);
+            setLoading(false);
+        })
+    }, []);
+
+    useEffect(() => {
+        loadData()
+    }, [loadData])
+
     return (
         <div className="container-fluid my-2">
             <div className="row">
-                <div className="col">
+                <div className="col-6 col-xm-12">
                     <ul className="list-group">
-                        <li className="list-group-item d-flex justify-content-between align-items-center">
-                            <Link to='/interview/amazon'> Amazon</Link>
-                            <span className="badge badge-primary badge-pill">14</span>
-                        </li>
-                        <li className="list-group-item d-flex justify-content-between align-items-center">
-                            <Link to='/interview/credit-suisse'> Credit-Suisse</Link>
-                            <span className="badge badge-primary badge-pill">2</span>
-                        </li>
-                        <li className="list-group-item d-flex justify-content-between align-items-center">
-                            <Link to='/article/microsoft'> Microsoft</Link>
-                            <span className="badge badge-primary badge-pill">1</span>
-                        </li>
-                        <li className="list-group-item d-flex justify-content-between align-items-center">
-                            <Link to='/article/sahaj'> Sahaj</Link>
-                            <span className="badge badge-primary badge-pill">5</span>
-                        </li>
-                        <li className="list-group-item d-flex justify-content-between align-items-center">
-                            <Link to='/article/ion'> ION</Link>
-                            <span className="badge badge-primary badge-pill">8</span>
-                        </li>
-                    </ul>
-                </div>
-                <div className="col">
-                    <ul className="list-group">
-                        <li className="list-group-item d-flex justify-content-between align-items-center">
-                            <Link to='/interview/barclays'> Barclays</Link>
-                            <span className="badge badge-primary badge-pill">14</span>
-                        </li>
-                        <li className="list-group-item d-flex justify-content-between align-items-center">
-                            <Link to='/interview/UBS'> UBS</Link>
-                            <span className="badge badge-primary badge-pill">6</span>
-                        </li>
-                        <li className="list-group-item d-flex justify-content-between align-items-center">
-                            <Link to='/article/HSBC'> HSBC</Link>
-                            <span className="badge badge-primary badge-pill">12</span>
-                        </li>
+                        {
+                            (loading) ? (
+                                <Loading />
+                            ) : (
+                                    companyList.map((item, index) => {
+                                        return (
+                                            <li key={index} className="list-group-item d-flex justify-content-between align-items-center" >
+                                                <Link to={`/interview/${item.company}`}> {item.company}</Link>
+                                                <span className="badge badge-secondary badge-pill">{item.count}</span>
+                                            </li>
+
+                                        )
+                                    })
+
+                                   
+                                )
+                        }
                     </ul>
                 </div>
             </div>
 
 
-        </div>
+
+        </div >
     )
 }
 
