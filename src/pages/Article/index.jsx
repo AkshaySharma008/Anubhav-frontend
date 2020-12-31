7834046f307cbc915943064238ca919c86f18d6d
Akashsaxena2308 <lordmartin2308@gmail.com>
2020-12-31 19:00:01 +0530

    Article Page v.1.0.0

--- src/pages/Article/index.jsx
+++ src/pages/Article/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import './index.scss';
 
 const Article = ({match}) => {
     return (
-        <div>
-            {match.params.articleId}
+        <div className="col-12 d-flex justify-content-around article-content">
+            <div className="flex-self-center">
+                <h1 className="content-heading mt-4 mb-1 col-12 text-left">Summer intern at Credit Suisse off-campus 2019</h1>
+                <p className="content-author-detail col-12 mb-4 text-left">Shailesh Kumar Sahu &nbsp; |&nbsp; 23 Dec 2020</p>
+                <p className="content-paragraph col-12 text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla numquam modi labore voluptatibus cum velit aperiam odio nisi in veritatis fuga id vero vel earum assumenda, dolor saepe ipsum quia a voluptatum! Cum eius iusto soluta enim pariatur. Est blanditiis itaque iusto praesentium. Iure reiciendis, velit, ab officia saepe eos atque eum fugiat qui recusandae obcaecati possimus laudantium nam natus eligendi expedita accusamus. Ipsum, ab, earum repellendus repudiandae saepe magni nulla, aliquam rem optio officiis rerum nesciunt.<br></br> Delectus, quas. Magnam illum provident quis nesciunt id delectus obcaecati consectetur perferendis unde beatae ullam, tenetur tempore minima impedit soluta repudiandae, enim facilis eaque voluptatibus cupiditate reprehenderit dignissimos voluptates.<br></br>  Architecto assumenda, laboriosam, tenetur odio sapiente tempora sed quia minus, dolore molestiae blanditiis? Quisquam animi quos blanditiis rerum aut neque quia, ut assumenda vitae sequi delectus illum, officiis nesciunt dicta quaerat. Autem nam libero, minima eius deserunt iure eos quidem voluptates sed fugiat vel rem rerum quis nulla quia nesciunt quasi corporis dolor! Eligendi blanditiis doloremque, et maxime quas nobis. Recusandae dolore autem voluptas? Deserunt soluta amet rem officiis quibusdam labore in veritatis quisquam deleniti ipsa<br></br>  nesciunt culpa aspernatur ullam enim consequatur unde dolore sapiente dicta, odio laboriosam ipsam! Ab est eius consequatur quod, ut rerum delectus nobis quasi adipisci voluptates totam quo omnis repudiandae aliquam illum, minima odio tenetur harum fuga fugiat sit quae sunt veniam. Placeat, fugiat velit! Expedita reiciendis maxime dolorum amet ducimus molestiae voluptatum<br></br>  quas ipsa cumque at illum praesentium corrupti accusamus omnis, nihil commodi nulla repellat totam molestias repellendus?</p>
+            </div>
         </div>
     )
 }
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -85,8 +85,6 @@ const HomePage = () => {
                                             </Link>
                                         )
                                     })
-
-
                                 )
                         }
 
