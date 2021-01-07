30f2a802fb74d1416df5e38d65517171f571b393
Akashsaxena2308 <lordmartin2308@gmail.com>
2021-01-08 00:06:33 +0530

    minor help

--- src/Component/feedbackModal/index.jsx
+++ src/Component/feedbackModal/index.jsx
@@ -6,7 +6,6 @@ import "./index.scss"
 export default class FeedbackModal extends Component {
     state = {
         iconClass: "far",
-
     }
 
     onClickHandler = () => {
@@ -34,23 +33,23 @@ render() {
                     
                     <input type="radio" id="dizzy" name="reaction" value="dizzy"></input>
 
-                    <label onClick={this.onClickHandler} htmlFor="dizzy"><i className={`far fa-dizzy display-4 m-2`} aria-hidden="true"></i></label>
+                    <label onClick={this.onClickHandler} htmlFor="dizzy"><i className={`${this.state.iconClass} fa-dizzy display-4 m-2`} aria-hidden="true"></i></label>
 
                     <input type="radio" id="frown" name="reaction" value="frown"></input>
 
-                    <label onClick={this.onClickHandler} htmlFor="frown"><i className={`far fa-frown-open display-4 m-2`} aria-hidden="true"></i></label>
+                    <label onClick={this.onClickHandler} htmlFor="frown"><i className={`${this.state.iconClass} fa-frown-open display-4 m-2`} aria-hidden="true"></i></label>
 
                     <input type="radio" id="meh" name="reaction" value="meh"></input>
 
-                    <label onClick={this.onClickHandler} htmlFor="meh"><i className="far fa-meh display-4 m-2" aria-hidden="true"></i></label>
+                    <label onClick={this.onClickHandler} htmlFor="meh"><i className={`${this.state.iconClass} fa-meh display-4 m-2`} aria-hidden="true"></i></label>
 
                     <input type="radio" id="happy" name="reaction" value="happy"></input>
 
-                    <label onClick={this.onClickHandler} htmlFor="happy"><i className={`far fa-grin-beam display-4 m-2`} aria-hidden="true"></i></label>
+                    <label onClick={this.onClickHandler} htmlFor="happy"><i className={`${this.state.iconClass}  fa-grin-beam display-4 m-2`} aria-hidden="true"></i></label>
 
                     <input type="radio" id="love" name="reaction" value="love"></input>
 
-                    <label onClick={this.onClickHandler} htmlFor="love"><i className={`far fa-grin-hearts display-4 m-2`} aria-hidden="true"></i></label>
+                    <label onClick={this.onClickHandler} htmlFor="love"><i className={`${this.state.iconClass}  fa-grin-hearts display-4 m-2`} aria-hidden="true"></i></label>
                     </form>
                     <br/>
                      Chituya
